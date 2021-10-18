160e6d45e48639b8a8c146bbd7f8dcf880f98845
AndreaTerenz <andrea.terenziani.at@gmail.com>
2021-10-18 23:21:46 +0200

    Fixed sceneHub bugs

--- Scripts/Globals.gd
+++ Scripts/Globals.gd
@@ -5,3 +5,10 @@ enum GROUPS  {
 	PLAYER,
 	FRIENDLY
 }
+
+func toggle_pause(val := 0):
+	if val != 0:
+		get_tree().paused = bool(val + 1)
+	else:
+		get_tree().paused = not(get_tree().paused)
+	VisualServer.set_shader_time_scale(0.0 if get_tree().paused else 1.0)
