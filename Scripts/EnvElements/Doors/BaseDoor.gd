5ac64444e709f1025f1e7b4334ffd80475d6cd0f
AndreaTerenz <andrea.terenziani.at@gmail.com>
2022-08-29 11:58:55 +0200

    Doors refactoring
    No sides reference in parent door, better signals

--- Scripts/EnvElements/Doors/BaseDoor.gd
+++ Scripts/EnvElements/Doors/BaseDoor.gd
@@ -1,43 +1,76 @@
 class_name BaseDoor
 extends Togglable
 
+# PORCALAMADONNA DIOBOIA
+enum TWEEN_TRANSITION {
+	LINEAR = 0,
+	SINE = 1,
+	QUINT = 2,
+	QUART = 3,
+	QUAD = 4,
+	EXPO = 5,
+	ELASTIC = 6,
+	CUBIC = 7,
+	CIRC = 8,
+	BOUNCE = 9,
+	BACK = 10,
+}
+
+# PORCALAMADONNA DIOBOIA PT 2
+enum TWEEN_EASE {
+	IN = 0,
+	OUT = 1,
+	IN_OUT = 2,
+	OUT_IN = 3
+}
+
+signal start_opening
 signal opened
+signal start_closing
 signal closed
 
 export(bool) var start_open := false
 
-export(float, .01, 10.0, .01) var speed = 1.0
+export(float, .01, 30.0, .01) var time = 1.0
 export(float, .2, 10.45, .05) var amount = 1.0
-export(NodePath) var left_side_path
-export(NodePath) var right_side_path
-
-onready var left_side: DoorSide = get_node(left_side_path)
-onready var right_side: DoorSide = get_node(right_side_path)
+export(TWEEN_TRANSITION) var transition_type = TWEEN_TRANSITION.LINEAR
+export(TWEEN_EASE) var ease_type = TWEEN_EASE.OUT
 
 var is_open := false
 
 func _ready() -> void:
-	left_side.door_ready(self)
-	right_side.door_ready(self)
-	
 	if start_open:
 		open()
 
 func open():
-	emit_signal("opened")
-	is_open = true
+	emit_signal("start_opening")
 	_on_open()
+	
+	yield(get_tree().create_timer(time), "timeout")
+	
+	is_open = true
+	emit_signal("opened")
 	
 func _on_open():
 	pass
 	
 func close():
-	emit_signal("closed")
-	is_open = false
+	emit_signal("start_closing")
 	_on_closed()
+	
+	yield(get_tree().create_timer(time), "timeout")
+	
+	is_open = false
+	emit_signal("closed")
 	
 func _on_closed():
 	pass
+	
+func switch():
+	if is_open:
+		close()
+	else:
+		open()
 	
 func _on_deactivated():
 	close()
--- Scripts/EnvElements/Doors/DoorSide.gd
+++ Scripts/EnvElements/Doors/DoorSide.gd
@@ -3,9 +3,12 @@ extends Spatial
 
 export(Vector3) var dir = Vector3.LEFT
 
+var parent_door : BaseDoor = null
 var amount: float = 0.0
+var tween_time: float = 0.0
 var mov_delta: Vector3 = Vector3.ZERO
-var speed: float = .1
+var tween_trns
+var tween_ease
 var base_pos: Vector3
 var open_pos: Vector3
 var current_trgt_pos: Vector3
@@ -16,6 +19,26 @@ var tween : Tween = null
 func _ready() -> void:
 	tween = Tween.new()
 	add_child(tween)
+	tween.connect("tween_completed", self, "_tween_done")
+	
+	if get_parent() is BaseDoor:
+		parent_door = get_parent()
+		
+		amount = parent_door.amount
+		tween_time = parent_door.time
+		tween_trns = parent_door.transition_type
+		tween_ease = parent_door.ease_type
+		
+		parent_door.connect("start_opening", self, "_on_open")
+		parent_door.connect("start_closing", self, "_on_close")
+		
+		base_pos = translation
+		current_trgt_pos = translation
+		
+		mov_delta = dir.normalized() * amount
+		open_pos = base_pos + mov_delta
+	else:
+		Console.Log.warn("DoorSide node '%s' isn't child of a BaseDoor node!" % get_path())
 
 func set_open(value):
 	open = value
@@ -24,27 +47,15 @@ func set_open(value):
 		current_trgt_pos = open_pos
 	else:
 		current_trgt_pos = base_pos
-		
-	tween_pos()
-		
-func tween_pos():
+	
+	tween.stop_all()
 	tween.interpolate_property(self, "translation", \
-		null, current_trgt_pos, amount / speed, \
-		Tween.TRANS_EXPO, Tween.EASE_OUT)
+		null, current_trgt_pos, tween_time, \
+		tween_trns, tween_ease)
 	tween.start()
-		
-func door_ready(door):
-	speed = door.speed / 10.0
-	amount = door.amount
 	
-	door.connect("opened", self, "_on_open")
-	door.connect("closed", self, "_on_close")
-	
-	base_pos = translation
-	current_trgt_pos = translation
-	
-	mov_delta = dir.normalized() * amount
-	open_pos = base_pos + mov_delta
+func _tween_done():
+	pass
 	
 func _on_open():
 	set_open(true)
